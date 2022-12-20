80cbc883c9c74fd60781417e5baea2e4c9e35776
Ashok <ashok@embin.com>
2022-12-20 16:21:42 +0530

    connection modiifed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,14 +6,29 @@ const mongoose = require("mongoose");
 //   //useCreateIndex: true,
 // });
 
-mongoose
-  .connect(process.env.DBConnection, {
-    // maxPoolSize: 50,
-    // wtimeoutMS: 2500,
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    //useFindAndModify: false,
-  })
-  .then(console.log("Database connected!"))
-  .catch((err) => console.log(`error >>: ${err}`));
-module.exports = mongoose;
+// mongoose
+//   .connect(process.env.DBConnection, {
+//     maxPoolSize: 50,
+//     wtimeoutMS: 2500,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     //useFindAndModify: false,
+//   })
+//   .then(console.log("Database connected!"))
+//   .catch((err) => console.log(`error >>: ${err}`));
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DBConnection, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+connectDB();
+//module.exports = connectDB;
